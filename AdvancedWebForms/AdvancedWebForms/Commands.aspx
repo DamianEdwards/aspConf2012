70bb24408675d811cf5edd0d9971b0ff288dc84b
DamianEdwards <damian@damianedwards.com>
2012-07-17 23:18:04 -0700

    Added the command pattern example. Deleted unused site.js files.

---
+++ AdvancedWebForms/AdvancedWebForms/Commands.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="Command Pattern" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Commands.aspx.cs" Inherits="AdvancedWebForms.Commands" %>
+
+<asp:Content ContentPlaceHolderID="MainContent" runat="server">
+    <hgroup class="title">
+        <h1><%: Page.Title %>.</h1>
+        <h2>Implementing commands via BubbleEvent.</h2>
+    </hgroup>
+
+    <asp:LinkButton runat="server" Text="Do Something"
+        CommandName="AdvancedWebForms.UpdateLabel"
+        CommandArgument="message" />
+
+    <asp:Button runat="server" Text="Do Something"
+        CommandName="AdvancedWebForms.UpdateLabel"
+        CommandArgument="message" />
+
+    <asp:Label runat="server" ID="message" />
+</asp:Content>
---
+++ AdvancedWebForms/AdvancedWebForms/Commands.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Compilation;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AdvancedWebForms
+{
+    public partial class Commands : CommandPage
+    {
+        // No code-behind in my page
+
+    }
+
+
+    public class UpdateLabelCommand : ICommand
+    {
+        public void Execute(Page page, Control source, object argument)
+        {
+            var target = source.NamingContainer.FindControl((string)argument) as Label;
+            if (target == null)
+            {
+                return;
+            }
+            target.Text = "I was updated by the UpdateLabelCommand via a " + source.GetType().Name;
+        }
+    }
+
+    public interface ICommand
+    {
+        void Execute(Page page, Control source, object argument);
+    }
+
+    public abstract class CommandPage : System.Web.UI.Page
+    {
+        protected override bool OnBubbleEvent(object source, EventArgs args)
+        {
+            var commandEventArgs = args as CommandEventArgs;
+            if (commandEventArgs != null)
+            {
+                // Find the ICommand
+                var commandType = BuildManager.GetType(commandEventArgs.CommandName + "Command", false);
+                if (commandType == null)
+                {
+                    return base.OnBubbleEvent(source, args);
+                }
+
+                var command = Activator.CreateInstance(commandType) as ICommand;
+                if (commandType == null)
+                {
+                    return base.OnBubbleEvent(source, args);
+                }
+
+                command.Execute(this, source as Control, commandEventArgs.CommandArgument);
+
+                // We handled the event, stop bubbling
+                return true;
+            }
+            return base.OnBubbleEvent(source, args);
+        }
+    }
+}
