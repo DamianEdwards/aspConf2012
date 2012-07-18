8aab65949f210c1197ca9d07be90069723690601
DamianEdwards <damian@damianedwards.com>
2012-07-18 00:19:58 -0700

    Added RequestValueProvider

---
+++ AdvancedWebForms/AdvancedWebForms/ModelBinding/RequestAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web;
+using System.Web.ModelBinding;
+
+namespace AdvancedWebForms.ModelBinding
+{
+    public class RequestAttribute : ValueProviderSourceAttribute
+    {
+        public override IValueProvider GetValueProvider(ModelBindingExecutionContext modelBindingExecutionContext)
+        {
+            return new RequestValueProvider(modelBindingExecutionContext);
+        }
+    }
+}
---
+++ AdvancedWebForms/AdvancedWebForms/ModelBinding/RequestValueProvider.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.ModelBinding;
+
+namespace AdvancedWebForms.ModelBinding
+{
+    public class RequestValueProvider : IValueProvider, IUnvalidatedValueProvider
+    {
+        private readonly List<IUnvalidatedValueProvider> _valueProviders = new List<IUnvalidatedValueProvider>();
+
+        public RequestValueProvider(ModelBindingExecutionContext modelBindingExecutionContext)
+        {
+            _valueProviders.Add(new QueryStringValueProvider(modelBindingExecutionContext));
+            _valueProviders.Add(new CookieValueProvider(modelBindingExecutionContext));
+            _valueProviders.Add(new FormValueProvider(modelBindingExecutionContext));
+        }
+
+        public bool ContainsPrefix(string prefix)
+        {
+            return _valueProviders.Any(vp => vp.ContainsPrefix(prefix));
+        }
+
+        public ValueProviderResult GetValue(string key)
+        {
+            return GetValue(key, false);
+        }
+
+        public ValueProviderResult GetValue(string key, bool skipValidation)
+        {
+            return _valueProviders.Select(vp => vp.GetValue(key, skipValidation))
+                .FirstOrDefault();
+        }
+    }
+}
